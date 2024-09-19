3eb68a3ec7216ccb5352f31ad07b6f241ff42e04
armaanhussain <145556287+armaanhussain@users.noreply.github.com>
2024-09-19 20:55:54 +0530

    music_store_analysis.sql

--- music_store_analysis.sql
+++ music_store_analysis.sql
@@ -1,4 +1,3 @@
-/*	Question Set 1 */
 
 /* Q1: Who is the senior most employee based on job title? */
 use music_store;
@@ -46,9 +45,7 @@ ORDER BY total_spending DESC
 LIMIT 1;
 
 
-/* Question Set 2  */
-
-/* Q1: Write query to return the email, first name, last name, & Genre of all Rock Music listeners. 
+/* Q6: Write query to return the email, first name, last name, & Genre of all Rock Music listeners. 
 Return your list ordered alphabetically by email starting with A. */
 
 
@@ -66,7 +63,7 @@ ORDER BY email;
 
 
 
-/* Q2: Let's invite the artists who have written the most rock music in our dataset. 
+/* Q7: Let's invite the artists who have written the most rock music in our dataset. 
 Write a query that returns the Artist name and total track count of the top 10 rock bands. */
 
 
@@ -82,7 +79,7 @@ LIMIT 10;
 
    
 
-/* Q3: Return all the track names that have a song length longer than the average song length. 
+/* Q8: Return all the track names that have a song length longer than the average song length. 
 Return the Name and Milliseconds for each track. Order by the song length with the longest songs listed first. */
 
 SELECT name,miliseconds
@@ -94,10 +91,7 @@ ORDER BY miliseconds DESC;
 
 
 
-
-/* Question Set 3 */
-
-/* Q1: Find how much amount spent by each customer on Best Selling Artist? Write a query to return customer name, artist name and total spent */
+/* Q9: Find how much amount spent by each customer on Best Selling Artist? Write a query to return customer name, artist name and total spent */
 
 
 
@@ -123,7 +117,7 @@ ORDER BY amount_spent DESC;
 
 
 
-/* Q2: We want to find out the most popular music Genre for each country. We determine the most popular genre as the genre 
+/* Q10: We want to find out the most popular music Genre for each country. We determine the most popular genre as the genre 
 with the highest amount of purchases. Write a query that returns each country along with the top Genre. For countries where 
 the maximum number of purchases is shared return all Genres. */
 
@@ -145,7 +139,7 @@ SELECT * FROM popular_genre WHERE RowNo <= 1 ;
 
 
 
-/* Q3: Write a query that determines the customer that has spent the most on music for each country. 
+/* Q11: Write a query that determines the customer that has spent the most on music for each country. 
 Write a query that returns the country along with the top customer and how much they spent. 
 For countries where the top amount spent is shared, provide all customers who spent this amount. */
 
